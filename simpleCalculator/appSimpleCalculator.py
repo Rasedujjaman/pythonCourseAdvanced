d801539c1b16841f0829450c077be1f47be9c860
Md Rasedujjaman <mrased01@gmail.com>
2021-09-08 14:06:19 +0200

    Update appSimpleCalculator.py

--- simpleCalculator/appSimpleCalculator.py
+++ simpleCalculator/appSimpleCalculator.py
@@ -9,7 +9,10 @@ LABEL_COLOR = '#25265E'
 SMALL_FONT_STYLE =("Arial", 16)
 LARGE_FONT_STYLE =("Arial", 40, "bold")
 DIGITS_FONT_STYLE = ("Arial", 24, "bold")
+DEFAULT_FONT_STYLE = ("Arial", 20)
 WHITE = '#FFFFFF'
+OFF_WHITE = '#F8FAFF'
+LIGHT_BLUE ='#CCEDFF'
 
 
 
@@ -21,7 +24,7 @@ class Calculator:
         self.window.title("Simple calculator")
         
         # the label 
-        self.total_expression = "0"
+        self.total_expression =   "0"
         self.current_expression = "0"
         
         
@@ -40,19 +43,51 @@ class Calculator:
             1:(3,1), 2:(3,2), 3:(3,3),
             0:(4, 2), '.':(4,1)
             }
+        
+        # Operation button
+        self.operations = {"/":"\u00F7", "*":"\u00D7", "-": "-", "+":"+"}
+
+        self.create_digit_buttons()
+        self.create_operator_buttons()
+        self.create_clear_button()
+        self.create_equals_button()
     
     
-        self.crate_digit_buttons()
+        #  Stretch the button to fill the button_frame
+        self.buttons_frame.rowconfigure(0, weight=1)
+        for x in range(1, 5):
+            self.buttons_frame.rowconfigure(x, weight=1)
+            self.buttons_frame.columnconfigure(x, weight=1)
+            
     
     
+    def create_clear_button(self):
+            button = tk.Button(self.buttons_frame, text="C", bg=LIGHT_BLUE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
+                               borderwidth=0)
+            button.grid(row = 0, column = 1, columnspan = 3, sticky = tk.NSEW)
+      
     
-    def crate_digit_buttons(self):
+    
+    def create_equals_button(self):
+        button = tk.Button(self.buttons_frame, text="=", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
+                           borderwidth=0)
+        button.grid(row = 4, column = 3, columnspan = 2, sticky = tk.NSEW)
+    
+    
+    def create_digit_buttons(self):
         for digit, gird_value in self.digits.items():
-            button = tk.Button(self.buttons_frame, text=str(digit), bg = WHITE, fg=LABEL_COLOR,font=DIGITS_FONT_STYLE)
+            button = tk.Button(self.buttons_frame, text=str(digit), bg = WHITE, fg=LABEL_COLOR,font=DIGITS_FONT_STYLE,
+                               borderwidth=0)
             button.grid(row=gird_value[0], column=gird_value[1], sticky=tk.NSEW)
         
     
-    
+    def create_operator_buttons(self):
+        i = 0
+        for operator, symbol in self.operations.items():
+            button = tk.Button(self.buttons_frame, text=symbol, bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
+                               borderwidth=0)
+            button.grid(row = i, column = 4, sticky = tk.NSEW)
+            i = i+1
     
     
     def create_display_labels(self):
