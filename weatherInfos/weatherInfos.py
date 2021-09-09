be42c57e9e9a42a6238bdf74534b4aad56ac2faa
Md Rasedujjaman <mrased01@gmail.com>
2021-09-09 17:42:00 +0200

    Create weatherInfos.py

---
+++ weatherInfos/weatherInfos.py
@@ -0,0 +1,14 @@
+
+# Here we will create a weather app using json and tkinter
+
+# JSON (JavaScript Object Notation), specified by RFC 7159 and
+# is a lightweight data interchange format inspired by JavaScript object literal syntax
+
+import json   
+from tkinter import *
+import os
+
+weather_api = os.environ['weatherAPI_key']
+
+print(weather_api)
+
